ff7d9f7e88b0553a8b9d9f34a0dcff8dea3a91c8
Ed Prince <edward_prince@hotmail.com>
2015-10-27 13:26:44 +0000

    build 1.1 variable assignment and syntax fixed

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,14 @@
+//Author: Ed Prince
+//Content: Web Scroll app.js
 $(document).ready(function(){
   main();
 });
 
 function main(){
-
-  //Any even ws id should be light (dark foreground)
-  //Any odd ws id should be dark (light foreground)
-  
-  //Auto gen all div id's
-  //Retrieve the number of ws divs and assign id's
-  //Initialize variables
   var id = $('.ws').id;
-  var evenDivs, oddDivs, length = $('body div:nth-child(even)');
+  var evenDivs = $('body div:nth-child(even)');
   var oddDivs = $('body div:nth-child(odd)');
-  var length = $('body div').length
+  var length = $('body div').length;
 
   for (x = 1; x <= length; x ++) {
     var el = $('body div:nth-child(' + x + ')');
@@ -21,14 +16,12 @@ function main(){
     if (x % 2 === 0) {
       //Even div, assign class
       el.addClass('even');
-      console.log("Added even");
     } else {
       el.addClass('odd');
-      console.log("Added odd");
     }
     el.append('<a></a>');
     $('body div:nth-child(' + x + ') a')
-      .attr('href', '#ws-' + (x + 1));
+      .attr('href', '#ws-' + (x + 1))
       .append('<i></i>');
     $('body div:nth-child(' + x + ') a i').addClass('ion-chevron-down');
 
