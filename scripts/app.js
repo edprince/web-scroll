0e27eb2a251506d2dcf88d427e8ccd376bb6581e
Ed Prince <edward_prince@hotmail.com>
2015-10-27 13:24:38 +0000

    build 1.0

--- scripts/app.js
+++ scripts/app.js
@@ -11,23 +11,25 @@ function main(){
   //Retrieve the number of ws divs and assign id's
   //Initialize variables
   var id = $('.ws').id;
-  var evenDivs = $('body div:nth-child(even)');
+  var evenDivs, oddDivs, length = $('body div:nth-child(even)');
   var oddDivs = $('body div:nth-child(odd)');
   var length = $('body div').length
 
   for (x = 1; x <= length; x ++) {
-    $('body div:nth-child(' + x + ')').attr('id', 'ws-' + x);
+    var el = $('body div:nth-child(' + x + ')');
+    el.attr('id', 'ws-' + x);
     if (x % 2 === 0) {
       //Even div, assign class
-      $('body div:nth-child(' + x + ')').addClass('even');
+      el.addClass('even');
       console.log("Added even");
     } else {
-      $('body div:nth-child(' + x + ')').addClass('odd');
+      el.addClass('odd');
       console.log("Added odd");
     }
-    $('body div:nth-child(' + x + ')').append('<a></a>');
-    $('body div:nth-child(' + x + ') a').attr('href', '#ws-' + (x + 1));
-    $('body div:nth-child(' + x + ') a').append('<i></i>');
+    el.append('<a></a>');
+    $('body div:nth-child(' + x + ') a')
+      .attr('href', '#ws-' + (x + 1));
+      .append('<i></i>');
     $('body div:nth-child(' + x + ') a i').addClass('ion-chevron-down');
 
   }
